a5e4ea0f71462d499422d1e3f6d7c52adbc06b6d
DavidTheMe <71494143+DavidTheMe@users.noreply.github.com>
2021-01-19 13:23:19 +0100

    challange 2

---
+++ Indiequest Challange 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private int cooldown = 0;
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown < 1)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 300;
+        }
+        cooldown -= 1;
+    }
+}
